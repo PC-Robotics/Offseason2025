3d6b666ef96f804a7037053f09fe0ed0e2ec3fa5
John Powers <john.powers@pinecrest.edu>
2025-03-02 21:25:08 -0500

    Added DriveBase and TeleopBase, default starting points for the simplest mecanum drive robot.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/DriveBase.java
@@ -0,0 +1,97 @@
+package org.firstinspires.ftc.teamcode.robots;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Simplistic drive base capable of using mecanum wheels and driving around
+ */
+public class DriveBase
+{
+    private LinearOpMode myOpMode = null;
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftRearDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightRearDrive = null;
+
+    public DriveBase (LinearOpMode opMode)
+    {
+        myOpMode = opMode;
+    }
+
+    /**
+     * Initializes the motors and sets their direction. Can also add other default settings.
+     *  - Set Mode: run with/without encoders
+     *  - Zero power behavior
+     */
+    public void init()
+    {
+        leftFrontDrive = myOpMode.hardwareMap.get(DcMotor.class, "leftFront");
+        leftRearDrive = myOpMode.hardwareMap.get(DcMotor.class, "leftRear");
+        rightFrontDrive = myOpMode.hardwareMap.get(DcMotor.class,"rightFront");
+        rightRearDrive = myOpMode.hardwareMap.get(DcMotor.class,"rightRear");
+
+
+        // ########################################################################################
+        // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
+        // ########################################################################################
+        // Most robots need the motors on one side to be reversed to drive forward.
+        // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
+        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
+        // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
+        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
+        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
+        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftRearDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightRearDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        myOpMode.telemetry.addData("Status","Initialized");
+        myOpMode.telemetry.update();
+
+    }
+
+    /**
+     * Standard POV Mecanum drive code
+     * @param axial left joystick y value
+     * @param lateral left joystick x value
+     * @param yaw right joystick x value
+     */
+    public void drive(double axial, double lateral, double yaw)
+    {
+        double max;
+
+        // Combine the joystick requests for each axis-motion to determine each wheel's power.
+        // Set up a variable for each drive wheel to save the power level for telemetry.
+        double leftFrontPower  = axial + lateral + yaw;
+        double rightFrontPower = axial - lateral - yaw;
+        double leftRearPower   = axial - lateral + yaw;
+        double rightRearPower  = axial + lateral - yaw;
+
+        // Normalize the values so no wheel power exceeds 100%
+        // This ensures that the robot maintains the desired motion.
+        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftRearPower));
+        max = Math.max(max, Math.abs(rightRearPower));
+
+        if(max > 1.0)
+        {
+            leftRearPower /= max;
+            leftFrontPower /= max;
+            rightRearPower /= max;
+            rightFrontPower /= max;
+        }
+
+        // Apply the power to the motors
+        leftFrontDrive.setPower(leftFrontPower);
+        leftRearDrive.setPower(leftRearPower);
+        rightFrontDrive.setPower(rightFrontPower);
+        rightRearDrive.setPower(rightRearPower);
+
+        // Add data to the telemetry for displaying the current motor powers
+        myOpMode.telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
+        myOpMode.telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftRearPower, rightRearPower);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopBase.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.robots.DriveBase;
+
+/**
+ * This op mode is for a simple base setup that uses just four motors and mecanum wheels
+ * For this code, there are no other motors or servos, and no odometry tracking
+ */
+@TeleOp(name="Basic Drive Train", group="Test Modes")
+public class TeleopBase extends LinearOpMode
+{
+    private DriveBase robot = new DriveBase(this);
+
+    public void runOpMode()
+    {
+        robot.init();
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            double axial = -gamepad1.left_stick_y;   // Forward on left stick yields negative val
+            double lateral = gamepad1.left_stick_x;
+            double yaw = gamepad1.right_stick_x;
+
+            robot.drive(axial,lateral,yaw);
+
+            telemetry.update();
+        }
+    }
+
+}
