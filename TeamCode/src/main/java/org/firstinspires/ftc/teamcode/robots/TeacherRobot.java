6703f5bf8ec75f3c1aadc699c708a74ec30250e0
MrLowePC <nicholas.lowe@pinecrest.edu>
2025-05-15 15:37:50 -0400

    Began building Teacher Robot Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/TeacherRobot.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.robots;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class TeacherRobot extends DriveBaseOdometry {
+
+    // Init motors
+    private CRServo intake = null;
+    private CRServo outtake = null;
+    private Servo tilt = null;
+    private Servo extender = null;
+    private DcMotor linearSlide = null;
+
+    // Drive constants
+    public static final double INTAKE_SPEED = 0;
+    public static final double OUTAKE_SPEED = 0;
+
+    public TeacherRobot (LinearOpMode opmode) {
+        super(opmode);
+    }
+
+    public void init(){
+        super.init();
+    }
+
+}
